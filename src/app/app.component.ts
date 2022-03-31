b88ec3928c8c9bd1d980a68962d53720ff21f965
Anastasios Theodosiou <anastasios.theodosiou@netcompany-intrasoft>
2022-03-31 18:20:41 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { config } from 'src/assets/config/cofig';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dynamic-env-vars';
+  results:any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(config.baseUrl).subscribe(res => {
+      this.results = res;
+      console.log(res);
+    })
+  }
 }
